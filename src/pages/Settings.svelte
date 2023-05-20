862a56ca89736abbce5508cc4cc18e3403da79f6
Trevor <trevor@osrs.tools>
2023-05-20 10:07:01 +0900

    Improve tabindex behavior for ledger csv import
    * ...but safari is broked

--- src/pages/Settings.svelte
+++ src/pages/Settings.svelte
@@ -5,6 +5,10 @@
   import { stringifyLedger, parseLedger } from 'src/lib/csv'
 
   let ledgerCsv = '#'
+  let ledgerDetailsOpen = false
+  /** @wtf export link is not focusable on Safari via keyboard navigation */
+  let ledgerExportTabindex = -1
+  let ledgerImportTabindex = -1
 
   function toggleCreditor(name: string) {
     const isPresent = !!$creditors.find(val => val === name)
@@ -36,6 +40,14 @@
       return `${count} ${textKind}`
     }).join(', ')
   })()
+
+  $: if (ledgerDetailsOpen) {
+    ledgerExportTabindex = 0
+    ledgerImportTabindex = 0
+  } else {
+    ledgerExportTabindex = -1
+    ledgerImportTabindex = -1
+  }
 
   async function onLedgerImport({ currentTarget }: { currentTarget: EventTarget & HTMLInputElement }) {
     const file = currentTarget.files[0]
@@ -79,7 +91,7 @@
           <h3 class='block text-sm font-medium leading-6 mb-1'>Import/Export</h3>
           <ul class='divide-y divide-gray-100 rounded-md border border-gray-200'>
             <li class='px-5 py-4 text-sm leading-6'>
-              <details class='flex items-center'>
+              <details class='flex items-center' bind:open={ledgerDetailsOpen}>
                 <summary class='flex justify-between expand-button rounded-sm px-3 py-2 text-sm font-medium ring-0 ring-inset ring-gray-900/10 shadow-sm hover:ring-1'>
                   <div class='flex gap-x-2'>
                     <Paperclip />
@@ -96,18 +108,21 @@
                     <p>This import replaces all transactions and accounts. Other app data is unaffected, so any account-related settings may no longer be applicable after importing.</p>
                   </div>
                   <div class='col-span-3 md:col-span-1 flex md:flex-col justify-around'>
-                    <a href={ledgerCsv} download=ledger.csv class='expand-button rounded-sm px-3 py-2 text-sm font-medium ring-0 ring-inset ring-gray-900/10 shadow-sm hover:ring-1'>
+                    <a tabindex={ledgerExportTabindex} href={ledgerCsv} download=ledger.csv class='expand-button rounded-sm px-3 py-2 text-sm font-medium ring-0 ring-inset ring-gray-900/10 shadow-sm hover:ring-1'>
                       Export
                     </a>
                     <label class='relative expand-button rounded-sm px-3 py-2 text-sm font-medium ring-0 ring-inset ring-gray-900/10 shadow-sm hover:ring-1'>
                       Import
-                      <input
-                        type='file'
-                        name='ledger-import'
-                        accept='text/csv'
-                        class='absolute opacity-0'
-                        on:change={onLedgerImport}
-                      />
+                      <span class='bring-back-browser-default-focus-style-because-opacity-hides-outline-colors-too absolute left-0 top-0 max-w-full h-full'>
+                        <input
+                          type='file'
+                          name='ledger-import'
+                          accept='text/csv'
+                          class='max-w-full h-full opacity-0'
+                          tabindex={ledgerImportTabindex}
+                          on:change={onLedgerImport}
+                        />
+                      </span>
                     </label>
                   </div>
                 </div>
@@ -200,5 +215,10 @@
 
   .expand-button:hover {
     color: var(--color-brand);
+  }
+
+  .bring-back-browser-default-focus-style-because-opacity-hides-outline-colors-too:focus-within {
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
   }
 </style>
